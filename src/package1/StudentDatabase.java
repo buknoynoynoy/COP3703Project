19089ad1ca2e7124443b40803901e7321fcc75c4
Vincent Almeda <vzire08@gmail.com>
2022-04-19 12:47:57 -0400

    commit -m "created working input for student"

--- src/package1/StudentDatabase.java
+++ src/package1/StudentDatabase.java
@@ -23,12 +23,38 @@ public class StudentDatabase {
     
             if (reachable) {
                 System.out.println("Connection Established");
+    
+                SQLInputs input = new SQLInputs();
+                Statement stmt = conn.createStatement();
+
+                String Fname = "Vincent";
+                String Minit = "R";
+                String Lname = "Almeda";
+                String Ssn = "987654321";
+                String Nnumber = "N12345678";
+                String Bdate = "2002-07-08";
+                String Sex = "M";
+                String sClass = "Sophomore";
+                String Degree = "BA";
+                String Cphone = "5202225866";
+                String Ccity = "Saint Johns";
+                String Cstate = "Florida";
+                String Czip = "32259";
+                String Pphone = "5202225866";
+                String Pcity = "Saint Johns";
+                String Pstate = "Florida";
+                String Pzip = "32259";
+                String studentInput = input.inputStudent(Fname, Minit, Lname, Ssn, Nnumber, Bdate, Sex, sClass, Degree, Cphone, Ccity, Cstate, Czip, Pphone, Pcity, Pstate, Pzip);
+                
+                System.out.println(studentInput);
+
+                ResultSet rset = stmt.executeQuery(studentInput);
+                conn.close();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        
-
+        //Connection conn = DriverManager.getConnection(url, userID, password);
     }
 }
