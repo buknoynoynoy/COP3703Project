7ac9e520f35431ee7ea7c85629bd5d85a7a0c547
Vincent Almeda <vzire08@gmail.com>
2022-04-19 11:39:31 -0400

    commit -m "created inputs.java"

---
+++ src/package1/SQLInputs.java
@@ -0,0 +1,13 @@
+package package1;
+import java.sql.*;
+import java.io.*;
+
+public class SQLInputs extends StudentDatabase {
+    public void inputStudent(String Fname, char Minit, String Lname, String ssn, 
+    String Nnumber, String Bdate, char Sex, String sClass, String Degree, String Cphone, 
+    String Ccity, String Cstate, String Czip, int Pphone, String Pcity, String Pstate, String Pzip) {
+        
+        //Statement stmt = conn.createStatement();
+         
+    }
+}
